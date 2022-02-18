fdc71cabfc1247a737af9d647c83742ecf40016e
Slava Kovalevskyi <vsk@fb.com>
2022-02-18 08:39:40 -0800

    working logic that supports local notebook execution

--- vai/cli.py
+++ vai/cli.py
@@ -1,20 +1,43 @@
 import argparse
 
-from vaip.model_builder import create_model
-from vaip.model_deployer import deploy_model
+from vai.prediction.model_builder import create_model
+from vai.prediction.model_deployer import deploy_model
+from vai.workbench.executor import execute_local_notebook
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="vaip",
-                                     description="GCP Vertex AI Model Publisher")
-    parser.add_argument("--tag", type=str, dest="tag", required=True,
-                                help="Docker Tag To Use With Build And/Or Deploy")
-    parser.add_argument("action", choices=["build", "deploy"],
+    parser = argparse.ArgumentParser(prog="vai",
+                                     description="GCP Vertex AI Workbench CLI")
+    parser.add_argument("action", choices=["build", "deploy", "execute-notebook"],
                                 help="action")
+    execute_notebook_group = parser.add_argument_group("execute-notebook")
+    deployer_group.add_argument("--project", type=str, dest="project",
+                                help="GCP Project (Vertex AI Workbench API should be enabled)")
+    deployer_group.add_argument("--location", type=str, dest="location",
+                                help="location should be supported by Vertex AI Workbench ")
+    deployer_group.add_argument("--gcs-folder", type=str, dest="gcs_folder",
+                                help="required for storing your notebook as well as the output")
+    deployer_group.add_argument("--notebook", type=str, dest="notebook",
+                                help="path for notebook to execute")
+    deployer_group.add_argument("--vm-type", type=str, dest="vm_type",
+                                help="type of VM to use for execution", default="n1-standard-4")
+    deployer_group.add_argument("--env-container", type=str, dest="env_container",
+                                help="Docker container, should be basead on GCP Deep Learnign Containers", 
+                                default="gcr.io/deeplearning-platform-release/base-cu110:latest")
+    deployer_group.add_argument("--kernel", type=str, dest="kernel",
+                                help="kernels to use, your container should be compatible with this kernel", 
+                                default="python3")
+    deployer_group.add_argument("--execution-id", type=str, dest="execution_id",
+                                help="execution id, should be unique or leave empty", 
+                                default="")
     builder_group = parser.add_argument_group("build")
+    builder_group.add_argument("--tag", type=str, dest="tag", required=True,
+                                help="Docker Tag To Use With Build And/Or Deploy")
     builder_group.add_argument("--path", type=str, dest="path", default=".",
                                 help="path to prediction.py file")
     deployer_group = parser.add_argument_group("deploy")
+    deployer_group.add_argument("--tag", type=str, dest="tag", required=True,
+                                help="Docker Tag To Use With Build And/Or Deploy")
     deployer_group.add_argument("--project", type=str, dest="project",
                                 help="GCP Project (Vertex AI API should be enabled)")
     deployer_group.add_argument("--location", type=str, dest="location",
@@ -28,3 +51,12 @@ def main():
         create_model(args.tag, args.path)
     if args.action == "deploy":
         deploy_model(args.project, args.location, args.name, args.tag)
+    if args.action == "execute-notebook":
+        execute_local_notebook(args.project, 
+                                args.location, 
+                                args.notebook, 
+                                args.gcs_folder, 
+                                args.execution_id, 
+                                args.env_container, 
+                                args.kernel, 
+                                args.vm_type)
--- vai/workbench/executor.py
+++ vai/workbench/executor.py
@@ -5,6 +5,24 @@ import google.auth.transport.requests
 import google.oauth2.id_token
 import uuid
 
+from google.cloud import storage
+
+
+def execute_local_notebook(gcp_project: str, 
+                            location: str, 
+                            input_notebook_file_path: str, 
+                            gcs_notebook_folder_path: str, 
+                            execution_id="", 
+                            env_uri="gcr.io/deeplearning-platform-release/base-cu110:latest", 
+                            kernel="python3", 
+                            master_type="n1-standard-4"):
+    gcs_bucket_name = _get_gcs_bucket_name_from_gcs_uri(gcs_notebook_folder_path)
+    file_name = input_notebook_file_path.split("/")[-1]
+    gcs_out_path = "/".join(gcs_notebook_folder_path.replace("gs://", "").split("/")[1:]) + "/" + file_name
+    input_gcs_notebook_path = f"gs://{gcs_notebook_folder_path}/{file_name}"
+    _upload_blob(gcp_project, gcs_bucket_name, input_notebook_file_path, gcs_out_path)
+    return execute_notebook(gcp_project, location, input_gcs_notebook_path, gcs_notebook_folder_path, execution_id, env_uri, kernel, master_type)
+
 
 def execute_notebook(gcp_project: str, 
                      location: str, 
@@ -30,21 +48,70 @@ def execute_notebook(gcp_project: str,
         }
     }
     data = json.dumps(values).encode('utf-8')
-    print(f"url to call: {service_url}")
-    print(f"data to send: {data}")
+    data_from_gcp = _send_generic_request(service_url, data)
+    if not data_from_gcp:
+        return None
+    print(str(data_from_gcp))
+    operation_uri = data_from_gcp["name"]
+    execution_uri = data_from_gcp["metadata"]["target"]
+    notebook_gcs_url = get_output_notebook_path(execution_uri)
+    notebook_gcs_url_without_scheme = notebook_gcs_url.replace("gs://", "")
+    viewer_url = f"https://notebooks.cloud.google.com/view/{notebook_gcs_url_without_scheme}"
+    return {
+        "operation_uri": operation_uri,
+        "execution_uri": execution_uri,
+        "notebook_gcs_url": notebook_gcs_url,
+        "viewer_url": viewer_url
+    }
+
+
+def get_output_notebook_path(execution_uri: str) -> str:
+    reuqest_url = f"https://notebooks.googleapis.com/v1/{execution_uri}"
+    response = _send_generic_request(reuqest_url)
+    return response["outputNotebookFile"]
+
+
+def _upload_blob(gcp_project, bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client(project=gcp_project)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+
+def _get_gcs_bucket_name_from_gcs_uri(gcs_uri):
+    return gcs_uri.split("/")[2]
+
+
+def _send_generic_request(url, data=None):
     creds, _ = google.auth.default()
     auth_req = google.auth.transport.requests.Request()
     creds.refresh(auth_req)
-    req = urllib.request.Request(service_url, data=data)
+    req = urllib.request.Request(url, data=data)
     req.add_header('Content-Type', 'application/json')
     req.add_header("Authorization", f"Bearer {creds.token}")
     response = urllib.request.urlopen(req)
+    if response.status != 200:
+        print(f"Error: {response.status}")
+        print(f"Error: {response.read()}")
+        return None
+    encoding = response.info().get_content_charset('utf-8')
+    return json.loads(response.read().decode(encoding))
 
-    return response.read()
 
-if "__main__" == __name__:
+# if "__main__" == __name__:
     # https://notebooks.googleapis.com/v1/projects/ml-lab-152505/locations/us-central1/executions?execution_id=3a5c9802-8f8d-11ec-b585-acde48001122
     # https://notebooks.googleapis.com/v1/projects/ml-lab-152505/locations/us-central1/executions?execution_id=0e05f924-8f8d-11ec-9223-acde48001122
     # :path: /aipn/v2/proxy/notebooks.googleapis.com%2Fv1%2Fprojects%2Fml-lab-152505%2Flocations%2Fus-central1%2Fexecutions%3Fexecution_id%3Duntitled__1645052627007?1645052645915
     # URL: https://7cc62a62987d13d7-dot-us-central1.notebooks.googleusercontent.com/aipn/v2/proxy/notebooks.googleapis.com%2Fv1%2Fprojects%2Fml-lab-152505%2Flocations%2Fus-central1%2Fexecutions%3Fexecution_id%3Duntitled__1645052627007?1645052645915
-    print(execute_notebook("ml-lab-152505", "us-central1", "gs://test-bucket-for-notebooks/executor_files/untitled__1645052627007/Untitled.ipynb", "gs://test-bucket-for-notebooks/executor_files/untitled__1645052627007", execution_id="untitiled123"))
+    # print(str(execute_notebook("ml-lab-152505", "us-central1", "gs://test-bucket-for-notebooks/executor_files/untitled__1645052627007/Untitled.ipynb", "gs://test-bucket-for-notebooks/executor_files/untitled__1645052627007")))
+    # notebook_gcs_url = get_output_notebook_path("projects/ml-lab-152505/locations/us-central1/executions/83214dfe-90d1-11ec-bd9d-acde48001122")
+    # notebook_gcs_url_without_scheme = notebook_gcs_url.replace("gs://", "")
+    # viewer_url = f"https://notebooks.cloud.google.com/view/{notebook_gcs_url_without_scheme}"
+    # print(viewer_url)
+    # print(execute_local_notebook("ml-lab-152505", "us-central1", "/Users/vsk/src/notebooks-ci-showcase/notebooks/clean.ipynb", "gs://test-bucket-for-notebooks/executor_files/"))
