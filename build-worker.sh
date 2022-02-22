376f9ee6cc5992bb18dd2253a2be0b31e6d9f86f
Slava Kovalevskyi <vsk@fb.com>
2022-02-22 10:33:11 -0800

    fix for the docker push

--- build-worker.sh
+++ build-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t us-west2-docker.pkg.dev/public-gcr/ai-workbench
-docker push -t us-west2-docker.pkg.dev/public-gcr/ai-workbench
+docker build . -t us-west2-docker.pkg.dev/public-gcr/ai-workbench/workbench-cli:latest
+docker push us-west2-docker.pkg.dev/public-gcr/ai-workbench/workbench-cli:latest
