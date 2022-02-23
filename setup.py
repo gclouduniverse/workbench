520e329110495b072af3ba847dd097f7f9ac5526
Slava Kovalevskyi <vsk@fb.com>
2022-02-23 14:24:46 -0800

    version 1.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 8):
 
 setup(
     name="ai-workbench",
-    version="1.2.0-SNAPSHOT",
+    version="1.2.0",
     author="Viacheslav Kovalevskyi",
     author_email="viacheslav@kovalevskyi.com",
     packages=find_packages(),
